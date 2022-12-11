6611a325ff50583420cadc63c1a781a0af1f1f0b
Clayton <snyderc@gmx.com>
2022-12-11 15:40:59 -0800

    Level end trigger

--- Source/Ballon/BallonGameModeBase.cpp
+++ Source/Ballon/BallonGameModeBase.cpp
@@ -4,6 +4,8 @@
 #include "BallonGameModeBase.h"
 
 #include "Blueprint/UserWidget.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/PawnMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 void ABallonGameModeBase::StartPlay()
@@ -18,12 +20,14 @@ void ABallonGameModeBase::DisplayLevelEndWidget(FLevelScore Score)
 {
 	ULevelEndWidget* NewWidget = CreateWidget<ULevelEndWidget>(GetWorld(), LevelEndClass);
 	NewWidget->AccuracyText = FString::Printf(
-		TEXT("%.1f%% Accuracy"), static_cast<float>(Score.NumPopped) / Score.ShotsFired * 100);
+		TEXT("%.1f%% Accuracy"),
+		Score.ShotsFired == 0 ? 0.0f : static_cast<float>(Score.NumPopped) / Score.ShotsFired * 100);
 	NewWidget->CompletionText = FString::Printf(TEXT("%d out of %d"), Score.NumPopped, Score.TotalPoppable);
 	NewWidget->TimeElapsedText = FString::Printf(TEXT("%.2fs"), Score.TimeElapsed);
 	NewWidget->AddToViewport();
 	APlayerController* Controller = UGameplayStatics::GetPlayerController(this, 0);
 	UGameplayStatics::GetPlayerPawn(this, 0)->DisableInput(Controller);
+	UGameplayStatics::SetGamePaused(this, true);
 	Controller->bShowMouseCursor = true;
 	NewWidget->bIsFocusable = true;
 	NewWidget->SetFocus();
